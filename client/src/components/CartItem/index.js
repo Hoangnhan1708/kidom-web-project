fe1cfebce8762de8a0994b123da537ad556b0ba9
Hoangnhan1708 <truongnhan115@gmail.com>
2024-05-20 15:48:47 +0700

    finish payment box

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -2,15 +2,16 @@ import classNames from 'classnames/bind';
 import { useState } from 'react';
 import Tippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css'; // optional
-
 import styles from './CartItem.module.scss';
 import InputQuantity from '../Layout/components/InputQuantity';
 import { ENV } from '~/env';
 const cx = classNames.bind(styles);
 
-function CartItem({ productId, img, name, price, oldQuantity }) {
-    const [quantity, setQuantity] = useState(oldQuantity);
+function CartItem({ item, onCheck }) {
+    const { productId, imageUrl, name, price, quantity, checked } = item;
+    const [__quantity, setQuantity] = useState(quantity);
     const [toogle, setToogle] = useState(false);
+
     const handleToogle = () => {
         setToogle(!toogle);
     };
@@ -34,21 +35,30 @@ function CartItem({ productId, img, name, price, oldQuantity }) {
             });
     };
 
+    const handleCheck = (e) => {
+        onCheck(productId, e.target.checked);
+    };
+
     return (
         <div className={`row align-items-center ${cx('wrapper')}`}>
-            <input type="checkbox" className={`col ${cx('checkbox')}`} />
+            <input
+                type="checkbox"
+                className={`col ${cx('checkbox', 'check-all')}`}
+                checked={checked}
+                onChange={handleCheck}
+            />
             <div className={`col ${cx('box')}`}>
-                <img className={cx('img')} src={img} alt={name} />
+                <img className={cx('img')} src={imageUrl} alt={name} />
             </div>
             <div className={`col ${cx('box')}`}>
                 <span className={`${cx('name')}`}>{name}</span>
             </div>
             <div className={`col ${cx('box')}`}>
+                {console.log(imageUrl)}
                 <span className={`${cx('price')}`}>₫{price}</span>
             </div>
-
             <div className="col">
-                <InputQuantity quantity={quantity} setQuantity={setQuantity} className="col" />
+                <InputQuantity quantity={__quantity} setQuantity={setQuantity} className="col" />
             </div>
             <div className={`col ${cx('box')}`}>
                 <span className={`${cx('price')}`}>₫{price * quantity}</span>
@@ -91,3 +101,101 @@ function CartItem({ productId, img, name, price, oldQuantity }) {
 }
 
 export default CartItem;
+
+// import classNames from 'classnames/bind';
+// import { useState } from 'react';
+// import Tippy from '@tippyjs/react/headless';
+// import 'tippy.js/dist/tippy.css'; // optional
+
+// import styles from './CartItem.module.scss';
+// import InputQuantity from '../Layout/components/InputQuantity';
+// import { ENV } from '~/env';
+// const cx = classNames.bind(styles);
+
+// function CartItem({ productId, img, name, price, oldQuantity, onCheck }) {
+//     const [quantity, setQuantity] = useState(oldQuantity);
+//     const [toogle, setToogle] = useState(false);
+//     const handleToogle = () => {
+//         setToogle(!toogle);
+//     };
+
+//     const handleDelete = () => {
+//         fetch(`http://localhost:5000/login/auth/me`, { credentials: 'include' })
+//             .then((res) => res.json())
+//             .then((me) => {
+//                 const userId = me.id;
+//                 fetch(`${ENV.SERVER_URL}/cart/delete-item`, {
+//                     method: 'POST',
+//                     headers: {
+//                         'Content-Type': 'application/json',
+//                     },
+//                     credentials: 'include',
+//                     body: JSON.stringify({ userId, productId }),
+//                 })
+//                     .then((res) => res.json())
+//                     .then((message) => alert(message.message))
+//                     .then(() => window.location.replace(`${ENV.BASE_URL}cart`));
+//             });
+//     };
+
+//     const handleCheck = (e) => {
+//         onCheck(productId, e.target.checked);
+//     };
+
+//     return (
+//         <div className={`row align-items-center ${cx('wrapper')}`}>
+//             <input type="checkbox" checked={checked} className={`col ${cx('checkbox')}`} />
+//             <div className={`col ${cx('box')}`}>
+//                 <img className={cx('img')} src={img} alt={name} />
+//             </div>
+//             <div className={`col ${cx('box')}`}>
+//                 <span className={`${cx('name')}`}>{name}</span>
+//             </div>
+//             <div className={`col ${cx('box')}`}>
+//                 <span className={`${cx('price')}`}>₫{price}</span>
+//             </div>
+
+//             <div className="col">
+//                 <InputQuantity quantity={quantity} setQuantity={setQuantity} className="col" />
+//             </div>
+//             <div className={`col ${cx('box')}`}>
+//                 <span className={`${cx('price')}`}>₫{price * quantity}</span>
+//             </div>
+//             <div className={`col ${cx('box')}`}>
+//                 <Tippy
+//                     render={(attrs) => (
+//                         <div className={cx('confirm-box')} tabIndex="-1" {...attrs}>
+//                             <span className={cx('confirm-title')}>Bạn chắc chắn muốn bỏ sản phẩm này?</span>
+//                             <div className={cx('btn-container')}>
+//                                 <button
+//                                     onClick={handleDelete}
+//                                     type="button"
+//                                     className={`btn btn-danger btn-lg ${cx('tippy-btn')}`}
+//                                 >
+//                                     Xóa
+//                                 </button>
+//                                 <button
+//                                     onClick={handleToogle}
+//                                     type="button"
+//                                     className={`btn btn-secondary btn-lg ${cx('tippy-btn')}`}
+//                                 >
+//                                     Hủy
+//                                 </button>
+//                             </div>
+//                         </div>
+//                     )}
+//                     placement="bottom"
+//                     visible={toogle}
+//                     interactive={true}
+//                     onClickOutside={handleToogle}
+//                 >
+//                     <button onClick={handleToogle} className={` ${cx('delete-btn')}`}>
+//                         Xóa
+//                     </button>
+//                 </Tippy>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default CartItem;
--- client/src/pages/Cart/index.js
+++ client/src/pages/Cart/index.js
@@ -1,13 +1,18 @@
 import classNames from 'classnames/bind';
-
-import styles from './Cart.module.scss';
+import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTicket } from '@fortawesome/free-solid-svg-icons';
 import CartItem from '~/components/CartItem';
+import styles from './Cart.module.scss';
 import { ENV } from '~/env';
-import { useEffect, useState } from 'react';
 const cx = classNames.bind(styles);
 
 function Cart({ user }) {
     const [items, setItems] = useState([]);
+    const [totalMoney, setTotalMoney] = useState(0);
+    const [checkedItemsCount, setCheckedItemsCount] = useState(0);
+    const [isAllChecked, setIsAllChecked] = useState(false);
+
     useEffect(() => {
         fetch(`http://localhost:5000/login/auth/me`, { credentials: 'include' })
             .then((res) => res.json())
@@ -19,19 +24,41 @@ function Cart({ user }) {
                     headers: {
                         'Content-Type': 'application/json',
                     },
-                    credentials: 'include', // If you need to include cookies in the request
+                    credentials: 'include',
                 })
                     .then((res) => res.json())
-                    .then((data) => setItems(data));
+                    .then((data) => setItems(data.map((item) => ({ ...item, checked: false }))));
             });
     }, []);
+
+    useEffect(() => {
+        const total = items.filter((item) => item.checked).reduce((sum, item) => sum + item.price * item.quantity, 0);
+        setTotalMoney(total);
+        setCheckedItemsCount(items.filter((item) => item.checked).length);
+    }, [items]);
+
+    const handleCheckAll = () => {
+        const newCheckedState = !isAllChecked;
+        setIsAllChecked(newCheckedState);
+        setItems(items.map((item) => ({ ...item, checked: newCheckedState })));
+    };
+
+    const handleItemCheck = (productId, checked) => {
+        setItems(items.map((item) => (item.productId === productId ? { ...item, checked } : item)));
+    };
+
     return (
         <div className={cx('wrapper')}>
             <h1 className={cx('heading')}>Giỏ hàng của bạn</h1>
             <div className={cx('inner')}>
                 <div className={cx('header')}>
                     <div className={`row align-items-center`}>
-                        <input type="checkbox" className={`col ${cx('checkbox')}`} />
+                        <input
+                            type="checkbox"
+                            className={`col ${cx('checkbox')}`}
+                            checked={isAllChecked}
+                            onChange={handleCheckAll}
+                        />
                         <div className={`col ${cx('box')}`}>
                             <span className={`${cx('title')}`}>Hình ảnh</span>
                         </div>
@@ -52,24 +79,157 @@ function Cart({ user }) {
                         </div>
                     </div>
                 </div>
-                {items ? (
-                    items.map((item) => (
-                        <CartItem
-                            productId={item.productId}
-                            key={item.productId}
-                            img={item.imageUrl}
-                            price={item.price}
-                            name={item.name}
-                            oldQuantity={item.quantity}
-                        />
-                    ))
+                {items.length ? (
+                    items.map((item) => {
+                        return <CartItem key={item.productId} item={item} onCheck={handleItemCheck} />;
+                    })
                 ) : (
-                    <div>Loading</div>
+                    <div>Loading...</div>
                 )}
-                <div className={cx('payment-container')}></div>
+                <div className={cx('payment-container')}>
+                    <div className={cx('payment-voucher')}>
+                        <FontAwesomeIcon className={cx('voucher-icon')} icon={faTicket} />
+                        Kidom voucher
+                    </div>
+                    <div className={cx('payment-total')}>
+                        <input
+                            type="checkbox"
+                            className={`${cx('checkbox')}`}
+                            checked={isAllChecked}
+                            onChange={handleCheckAll}
+                        />
+                        <label className={cx('payment-label')}>
+                            Tổng thanh toán: ({checkedItemsCount} sản phẩm): ₫{totalMoney}
+                        </label>
+                        <button className={cx('payment-btn')}>Mua hàng</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
 }
 
 export default Cart;
+
+// import classNames from 'classnames/bind';
+
+// import styles from './Cart.module.scss';
+// import CartItem from '~/components/CartItem';
+// import { ENV } from '~/env';
+// import { useEffect, useState } from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faTicket } from '@fortawesome/free-solid-svg-icons';
+// const cx = classNames.bind(styles);
+
+// function Cart({ user }) {
+//     const [items, setItems] = useState([]);
+//     const [totalMoney, setTotalMoney] = useState(0);
+//     const [checkedItemsCount, setCheckedItemsCount] = useState(0);
+//     const [isAllChecked, setIsAllChecked] = useState(false);
+
+//     useEffect(() => {
+//         fetch(`http://localhost:5000/login/auth/me`, { credentials: 'include' })
+//             .then((res) => res.json())
+//             .then((me) => {
+//                 const userId = me.id;
+
+//                 fetch(`${ENV.SERVER_URL}/cart/show?userId=${userId}`, {
+//                     method: 'GET',
+//                     headers: {
+//                         'Content-Type': 'application/json',
+//                     },
+//                     credentials: 'include', // If you need to include cookies in the request
+//                 })
+//                     .then((res) => res.json())
+//                     .then((data) => setItems(data.map((item) => ({ ...item, checked: false }))));
+//             });
+//     }, []);
+
+//     useEffect(() => {
+//         const total = items.filter((item) => item.checked).reduce((sum, item) => sum + item.price * item.quantity, 0);
+//         setTotalMoney(total);
+//         setCheckedItemsCount(items.filter((item) => item.checked).length);
+//     }, [items]);
+
+//     const handleCheckAll = () => {
+//         const newCheckedState = !isAllChecked;
+//         setIsAllChecked(newCheckedState);
+//         setItems(items.map((item) => ({ ...item, checked: newCheckedState })));
+//     };
+
+//     const handleItemCheck = (productId, checked) => {
+//         setItems(items.map((item) => (item.productId === productId ? { ...item, checked } : item)));
+//     };
+
+//     return (
+//         <div className={cx('wrapper')}>
+//             <h1 className={cx('heading')}>Giỏ hàng của bạn</h1>
+//             <div className={cx('inner')}>
+//                 <div className={cx('header')}>
+//                     <div className={`row align-items-center`}>
+//                         <input
+//                             type="checkbox"
+//                             checked={isAllChecked}
+//                             onChange={handleCheckAll}
+//                             className={`col ${cx('checkbox', 'check-all')}`}
+//                         />
+//                         <div className={`col ${cx('box')}`}>
+//                             <span className={`${cx('title')}`}>Hình ảnh</span>
+//                         </div>
+//                         <div className={`col ${cx('box')}`}>
+//                             <span className={`${cx('title')}`}>Sản phẩm</span>
+//                         </div>
+//                         <div className={`col ${cx('box')}`}>
+//                             <span className={`${cx('title')}`}>Đơn giá</span>
+//                         </div>
+//                         <div className={`col ${cx('box')}`}>
+//                             <span className={`${cx('title')}`}>Số lượng</span>
+//                         </div>
+//                         <div className={`col ${cx('box')}`}>
+//                             <span className={`${cx('title')}`}>Số tiền</span>
+//                         </div>
+//                         <div className={`col ${cx('box')}`}>
+//                             <span className={`${cx('title')}`}>Xóa</span>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 {items ? (
+//                     items.map((item) => {
+//                         return (
+//                             <CartItem
+//                                 productId={item.productId}
+//                                 key={item.productId}
+//                                 img={item.imageUrl}
+//                                 price={item.price}
+//                                 name={item.name}
+//                                 oldQuantity={item.quantity}
+//                                 onCheck={handleItemCheck}
+//                             />
+//                         );
+//                     })
+//                 ) : (
+//                     <div>Loading</div>
+//                 )}
+//                 <div className={cx('payment-container')}>
+//                     <div className={cx('payment-voucher')}>
+//                         <FontAwesomeIcon icon={faTicket} />
+//                         Kidom voucher
+//                     </div>
+//                     <div className={cx('payment-total')}>
+//                         <input
+//                             type="checkbox"
+//                             checked={isAllChecked}
+//                             onChange={handleCheckAll}
+//                             className={`${cx('checkbox')}`}
+//                         />
+//                         <label>
+//                             Tổng thanh toán: ({checkedItemsCount} sản phẩm): ₫{totalMoney}
+//                         </label>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Cart;
